d84d483090bcea526964bdf33eba3c342217694c
Luke Adams <luke@lukeclydeadams.com>
2021-04-09 11:43:32 -0600

    Fix formatting

--- src/JuliaGHA.jl
+++ src/JuliaGHA.jl
@@ -2,16 +2,18 @@ module JuliaGHA
 
 using DataStructures
 
-function add(x,y  )
-    return x +y+ 1
+export add, do_stuff
+
+function add(x, y)
+    return x + y + 1
 end
 
-function        do_stuff( )
-    queue = Queue{Int   }()
+function do_stuff()
+    queue = Queue{Int}()
 
-    push!(queue,1)
-    push!(queue,2)
-    push!(queue, "three"  )
+    push!(queue, 1)
+    push!(queue, 2)
+    push!(queue, "three")
 
     return queue
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using JuliaGHA
 using Test
 
 @testset "JuliaGHA.jl" begin
-    @test add(2, 2) = 4
+    @test add(2, 2) == 4
     @test dequeue!(do_stuff()) == 1
 end
