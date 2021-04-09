44bbafdda34fd050c49998ff7c91456be9d4e0a5
Luke Adams <luke@lukeclydeadams.com>
2021-04-09 10:43:00 -0600

    Add some poor formatting

--- src/JuliaGHA.jl
+++ src/JuliaGHA.jl
@@ -2,18 +2,18 @@ module JuliaGHA
 
 using DataStructures
 
-function add(x, y)
-    return x + y + 1
+function add(x,y  )
+    return x +y+ 1
 end
 
-function do_stuff()
-    queue = Queue{Int}()
+function        do_stuff( )
+    queue = Queue{Int   }()
 
-    push!(queue, 1)
-    push!(queue, 2)
-    push!(queue, "three")
+    push!(queue,1)
+    push!(queue,2)
+    push!(queue, "three"  )
 
     return queue
 end
 
-end
+end # module
