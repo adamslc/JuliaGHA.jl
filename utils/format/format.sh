24ba81f0141afe01c281a60fdccd11f50fe575e5
Luke Adams <luke@lukeclydeadams.com>
2021-04-09 11:11:22 -0600

    Use formatting script in CI

--- utils/format/format.sh
+++ utils/format/format.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 # Ensure that we are in the base directory
-if ![[ ${PWD##*/} == "JuliaGHA" ]]
+if [[ ${PWD##*/} != "JuliaGHA" ]]; then
 	echo 'This script should be run from the project root directory!'
-end
+fi
 
 # Instantiate the format enviroment and format all .jl and .md files in the project root directory.
 julia --project=utils/format -e 'using Pkg; Pkg.instantiate(); using JuliaFormatter; format(".", verbose=true)'
+
+echo 'Files changed:'
+echo '=============='
+git diff --name-only
