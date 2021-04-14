9a2efc6fcf34bad0f4220c02f9e1737f9bccde56
Luke Adams <luke@lukeclydeadams.com>
2021-04-14 17:30:12 -0600

    fail test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 using DataStructures
 
 using Logging: global_logger
-using GitHubActions: GitHubActionsLogger
-get(ENV, "GITHUB_ACTIONS", "false") == "true" && global_logger(GitHubActionsLogger())
+using GitHubActions
+get(ENV, "GITHUB_ACTIONS", "false") == "true" && global_logger(GitHubActions.GitHubActionsLogger())
 
 @debug "foo bar" rand(3)
 @info "foo bar" rand(3)
@@ -15,8 +15,11 @@ get(ENV, "GITHUB_ACTIONS", "false") == "true" && global_logger(GitHubActionsLogg
     @test add(2, 2) == 4
     @test dequeue!(do_stuff()) == 1
 
+    GitHubActions.start_group("Sub testset")
     @testset "Stuff" begin
         @test 1 + 1 == 2
+        @test 2 ^ 2 == 3
         @test 2 * 2 == 4
     end
+    GitHubActions.end_group()
 end
