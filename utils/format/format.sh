4469d812d25c724410178816a0471a52b0f1aa78
Luke Adams <luke@lukeclydeadams.com>
2021-04-14 17:04:10 -0600

    Fix formatting script

--- utils/format/format.sh
+++ utils/format/format.sh
@@ -2,14 +2,19 @@
 
 echo ${PWD##*/}
 
-# Ensure that we are in the base directory
-if !([[ ${PWD##*/} == "JuliaGHA" ]] || [[ ${PWD##*/} == "JuliaGHA.jl" ]]); then
-	echo 'This script should be run from the project root directory!'
-	exit 1
-fi
-
 # Instantiate the format enviroment and format all .jl and .md files in the project root directory.
-julia --project=utils/format -e 'using Pkg; Pkg.instantiate(); using JuliaFormatter; format(".", verbose=true)'
+julia --project=utils/format -e '
+directory = basename(pwd())
+if !(directory in ["JuliaGHA", JuliaGHA.jl"])
+	@error "Format script should be run the project root directory"
+	exit(1)
+end
+
+using Pkg
+Pkg.instantiate()
+
+using JuliaFormatter
+format(".", verbose=true)'
 
 echo 'Files changed:'
 echo '=============='
