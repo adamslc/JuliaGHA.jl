2f028e9a92d3cf99e087f6dc50350b7a05593200
Luke Adams <luke@lukeclydeadams.com>
2021-04-14 17:11:26 -0600

    Blah

--- utils/format/format.sh
+++ utils/format/format.sh
@@ -5,7 +5,7 @@ echo ${PWD##*/}
 # Instantiate the format enviroment and format all .jl and .md files in the project root directory.
 julia --project=utils/format -e '
 directory = basename(pwd())
-if !(directory in ["JuliaGHA", JuliaGHA.jl"])
+if !(directory in ["JuliaGHA", "JuliaGHA.jl"])
 	@error "Format script should be run the project root directory"
 	exit(1)
 end
--- utils/lint/lint.sh
+++ utils/lint/lint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# Ensure that we are in the base directory
-if [[ ${PWD##*/} != "JuliaGHA" ]]; then
-	echo 'This script should be run from the project root directory!'
-fi
-
 # Instantiate the format enviroment and format all .jl and .md files in the project root directory.
 JULIA_LOAD_PATH=utils/lint:.
 julia --project=utils/lint -e '
+directory = basename(pwd())
+if !(directory in ["JuliaGHA", JuliaGHA.jl"])
+	@error "Format script should be run the project root directory"
+	exit(1)
+end
+
 using Pkg
 Pkg.instantiate()
 using JET
