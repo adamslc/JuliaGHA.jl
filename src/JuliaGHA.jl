4a697acd3fe941e9ee578e583084910dabf11d69
Luke Adams <luke@lukeclydeadams.com>
2021-04-15 13:32:46 -0600

    long string

--- src/JuliaGHA.jl
+++ src/JuliaGHA.jl
@@ -11,6 +11,11 @@ end
 function do_stuff()
     queue = Queue{Int}()
 
+    str = "A really very super long string that I don't want to wrap for some reason. Don't ask me why. I don't know either. Blah blah blah."
+    asdf = 2
+    foo  = 3
+    a    = 4
+
     enqueue!(queue, 1)
     enqueue!(queue, 2)
     # enqueue!(queue, "three")
