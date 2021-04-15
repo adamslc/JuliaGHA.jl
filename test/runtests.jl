45eb6d6d1d7407c6a3705e4220ecdb4841db4676
Luke Adams <luke@lukeclydeadams.com>
2021-04-15 12:41:36 -0600

    make tests pass on master

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,6 @@ using GitHubActions
 get(ENV, "GITHUB_ACTIONS", "false") == "true" &&
     global_logger(GitHubActions.GitHubActionsLogger())
 
-@debug "foo bar" rand(3)
-@info "foo bar" rand(3)
-@warn "foo bar" rand(3)
-@error "foo bar" rand(3)
-
 @testset "JuliaGHA.jl" begin
     @test add(2, 2) == 4
     @test dequeue!(do_stuff()) == 1
@@ -19,7 +14,7 @@ get(ENV, "GITHUB_ACTIONS", "false") == "true" &&
     GitHubActions.start_group("Sub testset")
     @testset "Stuff" begin
         @test 1 + 1 == 2
-        @test 2^2 == 3
+        @test 2^2 == 4
         @test 2 * 2 == 4
     end
     GitHubActions.end_group()
