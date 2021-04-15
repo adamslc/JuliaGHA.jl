c2ed7d3411a6f45e052f7ab401990b0734239d11
Luke Adams <luke@lukeclydeadams.com>
2021-04-15 00:15:59 -0600

    logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,11 @@ using GitHubActions
 get(ENV, "GITHUB_ACTIONS", "false") == "true" &&
     global_logger(GitHubActions.GitHubActionsLogger())
 
-@testset "JuliaGHA.jl" begin
+using Pkg
+Pkg.dev(url="https://github.com/notinaboat/LoggingTestSets.jl")
+using LoggingTestSets
+
+@testset LoggingTestSet "JuliaGHA.jl" begin
     @test add(2, 2) == 4
     @test dequeue!(do_stuff()) == 1
 
@@ -22,11 +26,6 @@ get(ENV, "GITHUB_ACTIONS", "false") == "true" &&
     @testset "Badness" begin
         @test 1 + 1 == 2
         @test 1 - 1 == 0
-
-        try
-            sqrt(-1)
-        catch e
-            @error "asdfasdf" exception = (e, catch_backtrace())
-        end
+        @test 2 / 2 == 2
     end
 end
