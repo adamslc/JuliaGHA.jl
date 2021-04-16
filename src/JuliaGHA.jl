bbe3ff1c8b7dbec96a66a1ed12d1551b5b09f640
Luke Adams <luke@lukeclydeadams.com>
2021-04-15 23:43:35 -0600

    fix formatting

--- src/JuliaGHA.jl
+++ src/JuliaGHA.jl
@@ -13,8 +13,8 @@ function do_stuff()
 
     str = "A really very super long string that I don't want to wrap for some reason. Don't ask me why. I don't know either. Blah blah blah."
     asdf = 2
-    foo  = 3
-    a    = 4
+    foo = 3
+    a = 4
 
     enqueue!(queue, 1)
     enqueue!(queue, 2)
